874f7e294bbcd579bfca4a2128943f2ab3ca2569
SgaggeroAlessandro <14529841@itisrossi.vi.it>
2026-02-28 17:38:28 +0100

    Aggiungere i file di progetto.

---
+++ EsPuffi/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace EsPuffi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelAvvio = new System.Windows.Forms.Panel();
+            this.btn_Avvia = new System.Windows.Forms.Button();
+            this.pnlP = new System.Windows.Forms.Panel();
+            this.pnlC = new System.Windows.Forms.Panel();
+            this.pnlPuffo = new System.Windows.Forms.Panel();
+            this.btnA = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.btnW = new System.Windows.Forms.Button();
+            this.btnS = new System.Windows.Forms.Button();
+            this.lblR = new System.Windows.Forms.Label();
+            this.lblP = new System.Windows.Forms.Label();
+            this.panelAvvio.SuspendLayout();
+            this.pnlP.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelAvvio
+            // 
+            this.panelAvvio.Controls.Add(this.btn_Avvia);
+            this.panelAvvio.Location = new System.Drawing.Point(765, 463);
+            this.panelAvvio.Name = "panelAvvio";
+            this.panelAvvio.Size = new System.Drawing.Size(155, 61);
+            this.panelAvvio.TabIndex = 0;
+            // 
+            // btn_Avvia
+            // 
+            this.btn_Avvia.Location = new System.Drawing.Point(3, 3);
+            this.btn_Avvia.Name = "btn_Avvia";
+            this.btn_Avvia.Size = new System.Drawing.Size(151, 55);
+            this.btn_Avvia.TabIndex = 0;
+            this.btn_Avvia.Text = "Avvia gioco";
+            this.btn_Avvia.UseVisualStyleBackColor = true;
+            this.btn_Avvia.Click += new System.EventHandler(this.btn_Avvia_Click);
+            // 
+            // pnlP
+            // 
+            this.pnlP.Controls.Add(this.pnlC);
+            this.pnlP.Controls.Add(this.pnlPuffo);
+            this.pnlP.Location = new System.Drawing.Point(12, 12);
+            this.pnlP.Name = "pnlP";
+            this.pnlP.Size = new System.Drawing.Size(736, 540);
+            this.pnlP.TabIndex = 1;
+            // 
+            // pnlC
+            // 
+            this.pnlC.Location = new System.Drawing.Point(344, 118);
+            this.pnlC.Name = "pnlC";
+            this.pnlC.Size = new System.Drawing.Size(109, 117);
+            this.pnlC.TabIndex = 1;
+            // 
+            // pnlPuffo
+            // 
+            this.pnlPuffo.Location = new System.Drawing.Point(20, 21);
+            this.pnlPuffo.Name = "pnlPuffo";
+            this.pnlPuffo.Size = new System.Drawing.Size(93, 120);
+            this.pnlPuffo.TabIndex = 0;
+            // 
+            // btnA
+            // 
+            this.btnA.Location = new System.Drawing.Point(754, 237);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(50, 50);
+            this.btnA.TabIndex = 2;
+            this.btnA.Text = "A";
+            this.btnA.UseVisualStyleBackColor = true;
+            this.btnA.Click += new System.EventHandler(this.btnA_Click);
+            // 
+            // btnD
+            // 
+            this.btnD.Location = new System.Drawing.Point(884, 237);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(50, 50);
+            this.btnD.TabIndex = 3;
+            this.btnD.Text = "D";
+            this.btnD.UseVisualStyleBackColor = true;
+            this.btnD.Click += new System.EventHandler(this.btnD_Click);
+            // 
+            // btnW
+            // 
+            this.btnW.Location = new System.Drawing.Point(821, 181);
+            this.btnW.Name = "btnW";
+            this.btnW.Size = new System.Drawing.Size(50, 50);
+            this.btnW.TabIndex = 4;
+            this.btnW.Text = "W";
+            this.btnW.UseVisualStyleBackColor = true;
+            this.btnW.Click += new System.EventHandler(this.btnW_Click);
+            // 
+            // btnS
+            // 
+            this.btnS.Location = new System.Drawing.Point(821, 293);
+            this.btnS.Name = "btnS";
+            this.btnS.Size = new System.Drawing.Size(50, 50);
+            this.btnS.TabIndex = 5;
+            this.btnS.Text = "S";
+            this.btnS.UseVisualStyleBackColor = true;
+            this.btnS.Click += new System.EventHandler(this.btnS_Click);
+            // 
+            // lblR
+            // 
+            this.lblR.AutoSize = true;
+            this.lblR.Location = new System.Drawing.Point(807, 39);
+            this.lblR.Name = "lblR";
+            this.lblR.Size = new System.Drawing.Size(71, 16);
+            this.lblR.TabIndex = 6;
+            this.lblR.Text = "Punteggio:";
+            // 
+            // lblP
+            // 
+            this.lblP.AutoSize = true;
+            this.lblP.Location = new System.Drawing.Point(814, 89);
+            this.lblP.Name = "lblP";
+            this.lblP.Size = new System.Drawing.Size(0, 16);
+            this.lblP.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1187, 727);
+            this.Controls.Add(this.lblP);
+            this.Controls.Add(this.lblR);
+            this.Controls.Add(this.btnS);
+            this.Controls.Add(this.btnW);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.pnlP);
+            this.Controls.Add(this.panelAvvio);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            
+            this.panelAvvio.ResumeLayout(false);
+            this.pnlP.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelAvvio;
+        private System.Windows.Forms.Button btn_Avvia;
+        private System.Windows.Forms.Panel pnlP;
+        private System.Windows.Forms.Panel pnlPuffo;
+        private System.Windows.Forms.Panel pnlC;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.Button btnW;
+        private System.Windows.Forms.Button btnS;
+        private System.Windows.Forms.Label lblR;
+        private System.Windows.Forms.Label lblP;
+    }
+}
+
---
+++ EsPuffi/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace EsPuffi
+{
+    public partial class Form1 : Form
+    {
+        int p = 0;
+        int velocità = 10;
+        int l = 110;
+        int h = 120;
+       
+
+        struct Punto
+        {
+            public int x;
+            public int y;
+
+            public Punto(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            panelAvvio.Visible = true;
+            pnlP.Visible = false;
+            btnA.Visible = false;
+            btnW.Visible = false; 
+            btnD.Visible = false;
+            btnS.Visible = false;
+            lblR.Visible = false;
+            Punto punto = new Punto();
+            Random rnd = new Random();
+             
+            int Xmax = pnlP.Width - l;
+            int Ymax = pnlP.Height - h;
+            punto.x = rnd.Next(0, Xmax);
+            punto.y = rnd.Next(0, Ymax);
+
+            pnlC.Location = new Point(punto.x, punto.y);
+        }
+
+        private void btn_Avvia_Click(object sender, EventArgs e)
+        {
+            panelAvvio.Visible = false;
+            pnlP.Visible = true;
+            pnlC.Visible = true;
+            pnlC.BackColor = Color.Red;
+            pnlPuffo.Visible = true;
+            pnlPuffo.BackColor = Color.Blue;
+            btnA.Visible = true;
+            btnW.Visible = true;
+            btnS.Visible = true;
+            btnD.Visible = true;
+            lblR.Visible = true;
+            lblP.Text = p.ToString();
+        }
+
+        private void btnW_Click(object sender, EventArgs e)
+        {
+            if(pnlPuffo.Location.Y - velocità < 0)
+            {
+                pnlPuffo.Location = new Point(pnlPuffo.Location.X, 0);
+
+            }
+            else
+            {
+                pnlPuffo.Location = new Point(pnlPuffo.Location.X, pnlPuffo.Location.Y - velocità);
+            }
+            casettaraggiunta();
+
+        }
+
+        private void btnA_Click(object sender, EventArgs e)
+        {
+            if (pnlPuffo.Location.X - velocità < 0)
+            {
+                pnlPuffo.Location = new Point(0, pnlPuffo.Location.Y);
+
+            }
+            else
+            {
+                pnlPuffo.Location = new Point(pnlPuffo.Location.X - velocità, pnlPuffo.Location.Y);
+            }
+            
+            casettaraggiunta();
+             
+        }
+
+        private void btnD_Click(object sender, EventArgs e) //correggere
+        {
+            int maxX = pnlP.Width - pnlPuffo.Width;
+            if (pnlPuffo.Location.X + velocità >= maxX)
+            {
+                pnlPuffo.Location = new Point(maxX, pnlPuffo.Location.Y);
+
+            }
+            else
+            {
+                pnlPuffo.Location = new Point(pnlPuffo.Location.X + velocità, pnlPuffo.Location.Y);
+            }
+           
+            casettaraggiunta();
+           
+           
+        }
+
+        private void btnS_Click(object sender, EventArgs e) //corregere
+        {
+            int maxY = pnlP.Height - pnlPuffo.Height;
+            if (pnlPuffo.Location.Y + velocità >= maxY)
+            {
+                pnlPuffo.Location = new Point(pnlPuffo.Location.X, maxY);
+
+            }
+            else
+            {
+                pnlPuffo.Location = new Point(pnlPuffo.Location.X, pnlPuffo.Location.Y + velocità);
+            }
+            
+            casettaraggiunta();
+            
+        }
+        private void casettaraggiunta()
+
+        {
+            int puffoDx = pnlPuffo.Location.X + pnlPuffo.Width;  
+            int puffoGiu = pnlPuffo.Location.Y + pnlPuffo.Height; 
+
+            
+            int ciboDx = pnlC.Location.X + pnlC.Width;   
+            int ciboGiu = pnlC.Location.Y + pnlC.Height; 
+            if (pnlPuffo.Location.X < ciboDx && puffoDx > pnlC.Location.X && pnlPuffo.Location.Y < ciboGiu && puffoGiu > pnlC.Location.Y)
+            {
+                p++;
+                lblP.Text = p.ToString();
+                Random rnd = new Random();
+                int Xmax = pnlP.Width - l;
+                int Ymax = pnlP.Height - h;
+                int x = rnd.Next(0, Xmax);
+                int y = rnd.Next(0, Ymax);
+                pnlC.Location = new Point(x, y);
+            }
+            
+        }
+
+       
+    }
+}
